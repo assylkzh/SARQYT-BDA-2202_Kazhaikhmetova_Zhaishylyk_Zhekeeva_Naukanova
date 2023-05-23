2ccc743de4e66cbfdcca76265791344aa8114f21
assylkzh <akazhiakhmetova05@mail.ru>
2023-05-23 19:56:13 +0600

    "

--- interior.js
+++ interior.js
@@ -1,8 +1,10 @@
+// popover
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
       var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl)
 });
 
+// slideBar
 function openNav() {
   document.getElementById("Sidebar").style.width = "400px";
   document.getElementById("main").style.marginLeft = "400px";
@@ -13,6 +15,7 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
+// music
 var audio = document.getElementById("music");
 var playButton = document.getElementById("playButton");
 var playIcon = document.getElementById("playIcon");
